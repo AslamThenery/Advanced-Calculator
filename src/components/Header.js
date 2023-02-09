2293cd534487e446ad68a01c58874845543f6c9d
Muhammed Aslam <aslamccmc@gmail.com>
2023-02-09 21:07:43 +0530

    Header Completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import {React, useState} from 'react'
 import "./header.css"
+
 function Header() {
+
+
+  const [clCalculator, setClCalculator] = useState("active-button")
+  const [clConverter, setClConverter] = useState("inactive-button")
+  const handleCalculator= ()=>{
+        
+          setClCalculator("active-button")
+      setClConverter("inactive-button")
+         
+        }
+   const handleConverter = ()=>{
+      setClCalculator("inactive-button")
+      setClConverter("active-button")
+         
+   }       
+  
   return (
+
+
+  
+    
     <div className='header'>
-        <button className='calc'>Calculator</button>
-        <button className='converter'>Coverter</button>
+        {/* <button className="active-button" onClick={handleClass}>Calculator</button> */}
+        <button className={`${clCalculator}`} onClick={handleCalculator}>Calculator</button>
+        {/* <button className='inactive-button'>Coverter</button> */}
+        <button className={ `${clConverter}`} onClick={handleConverter}>Coverter</button>
+        
     </div>
   )
 }
