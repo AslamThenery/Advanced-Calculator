974df100ca601960d73adb4d9b5939db8685cb75
Muhammed Aslam <aslamccmc@gmail.com>
2023-02-10 00:24:15 +0530

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,28 @@ function Header() {
 
   const [clCalculator, setClCalculator] = useState("active-button")
   const [clConverter, setClConverter] = useState("inactive-button")
+  const [clArea, setClArea] = useState("inactive-button")
   const handleCalculator= ()=>{
         
           setClCalculator("active-button")
       setClConverter("inactive-button")
+      setClArea("inactive-button")
+      
          
         }
    const handleConverter = ()=>{
       setClCalculator("inactive-button")
+      setClArea("inactive-button")
       setClConverter("active-button")
          
-   }       
+   }   
+   
+   const handleArea = () =>{
+    setClCalculator("inactive-button")
+    setClArea("active-button")
+    setClConverter("inactive-button")
+
+   }
   
   return (
 
@@ -28,6 +39,7 @@ function Header() {
         <button className={`${clCalculator}`} onClick={handleCalculator}>Calculator</button>
         {/* <button className='inactive-button'>Coverter</button> */}
         <button className={ `${clConverter}`} onClick={handleConverter}>Coverter</button>
+        <button className={ `${clArea}`} onClick={handleArea}>Area Calculator</button>
         
     </div>
   )
